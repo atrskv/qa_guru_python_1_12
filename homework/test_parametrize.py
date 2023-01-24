02bfb4896d97e19ceedc3656f41f92d4db1ec4c6
Aleksei <trrskv@gmail.com>
2023-01-24 23:06:15 +0300

    Add homework

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -1,17 +1,63 @@
 """
 Переопределите параметр с помощью indirect
 """
+
 import pytest
+from selene import be
+from selene.support.shared import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[('1920', '1080'),
+                        ('1024', '720'),
+                        ('393', '851'),
+                        ('390', '844')])
+def browser_management(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+
+    yield
+
+    browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.mark.parametrize('browser_management', [('1920', '1080')], indirect=True)
+def test_github_desktop(browser_management):
+
+    # GIVEN:
+    browser.open('https://github.com/')
+
+    # WHEN:
+    sign_in_button = (
+        browser.element('[class*="sign-in"]')
+        .click()
+    )
+
+    # THEN:
+    submit_button = (
+        browser.element('[class$="sign-in-button"]')
+        .should(be.visible)
+    )
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('browser_management', [('390', '844')], indirect=True)
+def test_github_mobile(browser_management):
+
+    # GIVEN:
+    browser.open('https://github.com/')
+
+    # WHEN:
+    hamburger_menu = (
+        browser.element('.HeaderMenu-toggle-bar')
+        .click()
+    )
+
+    sign_in_button = (
+        browser.element('[class*="sign-in"]')
+        .click()
+    )
+
+    # THEN:
+    submit_button = (
+        browser.element('[class$="sign-in-button"]')
+        .should(be.visible)
+    )
